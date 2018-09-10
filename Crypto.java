376e7f42d9b52f6010bbf4e8298cc21d20302d01
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2018-09-10 11:59:01 -0500

    Add files via upload

---
+++ Crypto.java
@@ -0,0 +1,99 @@
+import java.io.*;
+/**
+ * 
+ * @author Andrés Guillermo Bonilla Olarte
+ *
+ */
+public class Crypto
+{
+    public static String convertVocals(String original)
+    {
+        String modified = original;
+        modified = modified.replace("a","4"); //replaces "a" to "4"
+        modified = modified.replace("e","3"); //replaces "e" to "3"
+        modified = modified.replace("i","1"); //replaces "i" to "1"
+        modified = modified.replace("o","0"); //replaces "o" to "0"
+        modified = modified.replace("u","9"); //replaces "u" to "9"
+        modified = modified.replace("\\s+","\\s+");
+        
+        return modified;
+    }
+    
+    /**
+     * 
+     * @param real
+     * @return
+     */
+    
+    public static String convertVowels(String real)
+    {
+        String modified = real;
+        modified = modified.replace("4","a"); //replaces "4" to "a"
+        modified = modified.replace("3","e"); //replaces "3" to "e"
+        modified = modified.replace("1","i"); //replaces "1" to "i"
+        modified = modified.replace("0","o"); //replaces "0" to "o"
+        modified = modified.replace("9","u"); //replaces "9" to "u"
+        modified = modified.replace("\\s+","\\s+");
+        
+        return modified;
+    }
+    
+    /**
+     * 
+     * @param args
+     */
+
+    public static void main(String[] args) 
+    {
+        BufferedReader br = new BufferedReader (new InputStreamReader (System.in));       //creating the Reading Buffer
+        BufferedWriter bw = new BufferedWriter (new OutputStreamWriter (System.out));     //creating the Writing Buffer
+        try
+        {
+        	
+        	String Cadena = br.readLine();  //reading the string
+        	String cadena = Cadena.toLowerCase();  //converting all the string to lower characters
+        	String c4d3n4 = convertVocals(cadena); //convert all vocals into numbers, as the function
+
+        	char encrypt[] = c4d3n4.toCharArray();
+      		char decrypt[] = cadena.toCharArray();
+      		
+        	bw.write("Texto original: " + "\n");  //showing the original string
+        	bw.write(cadena + "\n");
+        	
+        	bw.write("Texto con vocales convertidas: " + "\n");
+        	bw.write(c4d3n4 + "\n");
+        	
+        	  //creating the split substring
+          
+        	bw.write("Texto encriptado:"+"\n"); //showing the new string
+        	
+        	 for (int i = 0; i < encrypt.length; i++)
+       		 {
+       			 encrypt[i] = (char)(encrypt[i] + (char) 5) ;
+       		 }
+
+        	 String encrypted = String.valueOf(encrypt);
+        	 String[] splitted = encrypted.split("\\s+");
+        	 bw.write(splitted);
+        	 bw.newLine();
+       		 
+       		 bw.write("Texto desencriptado:" + "\n");
+       		 
+       		 for (int j = 0; j < decrypt.length; j++)
+       		 {
+       			 decrypt[j] = (char)(encrypt[j] - (char) 5) ; 
+       		 }
+       		 String decrypted = String.valueOf(decrypt);
+       		 String desencriptado = convertVowels(decrypted);
+  			 bw.write(desencriptado);
+       		 bw.newLine();
+      
+       		 bw.flush(); //releases information
+	 
+        }
+        catch (Exception ex)
+        {
+        	ex.printStackTrace();
+        }
+    }
+}
