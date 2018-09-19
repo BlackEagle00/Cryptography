4272e84d833a7c401dfc0955269800f9ea9602ca
Andrés Bonilla <42350758+BlackEagle00@users.noreply.github.com>
2018-09-18 21:20:33 -0500

    Update Crypto.java

--- Crypto.java
+++ Crypto.java
@@ -1,8 +1,10 @@
 import java.io.*;
+/*
+# Cryptography is free software: you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation, version 3.
+*/
 /**
- * 
  * @author Andrés Guillermo Bonilla Olarte
- *
  */
 public class Crypto
 {
